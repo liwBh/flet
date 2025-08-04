7bee10d97ddd219299e5ccc0fbcd68011d042f0d
liwbh <liwbarqueroh@gmail.com>
2025-08-04 10:31:57 -0600

    updated main

--- trello/src/main.py
+++ trello/src/main.py
@@ -3,9 +3,14 @@ from components.layout.navbar import Navbar
 from components.layout.layout import Layout
 
 def main(page: ft.Page):
+    # settings
     page.title = "Flet Trello clone"
     page.padding = 0
     page.bgcolor = ft.Colors.BLUE_GREY_500
+    page.theme = ft.Theme(font_family="Verdana")
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.theme.page_transitions.windows = ft.PageTransitionTheme.CUPERTINO
+    page.fonts = {"Pacifico": "./fonts/Pacifico-Regular.ttf"}
 
     navbar = Navbar(page)
     layout = Layout(navbar, page)
@@ -20,4 +25,4 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
-    ft.app(main)
+    ft.app(target=main, assets_dir="./assets")
